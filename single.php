67d8ad39e24d40921608f2bff389e8b7bbedab62
uxte <pedro.reis@uxte.com>
2020-06-26 15:37:14 +0100

    Project page

--- single.php
+++ single.php
@@ -4,12 +4,15 @@
         <section class="project">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
             $post_cover_text = get_post_meta( $post->ID, 'cover_meta', 1);
+            if ( empty($post_cover_text) ) {
+                $post_cover_text = get_the_excerpt();
+            };
             $post_color = get_post_meta( $post->ID, 'color_meta', 1);
             $post_style = 'style="background-color:' . $post_color . '"';
             ?>
             <header <?php print $post_style; ?>>
                 <span class="wrapper">With <h1><?php the_title(); ?></h1></span>
-                <h2><?php print $post_cover_text ?></h2>
+                <h2><?php print $post_cover_text; ?></h2>
             </header>
                 <?php the_content(); ?>
             <?php endwhile; endif; ?>
--- template-parts/sidebar.php
+++ template-parts/sidebar.php
@@ -5,8 +5,12 @@
             $next_post_title = $next_post->post_title;
             $next_post_link = get_permalink( $next_post->ID );
             $next_post_cover_text = get_post_meta( $next_post->ID, 'cover_meta', 1 );
+            if ( empty( $next_post_cover_text ) ) {
+                $next_post_cover_text = get_the_excerpt( $next_post->ID );
+            };
         ?>
         <a class="next-post" href="<?php echo $next_post_link; ?>">
+            <span class="caption">Next project:</span>
             <span class="title">With <strong><?php print $next_post_title; ?></strong></span>
             <span class="cover-text"><?php print $next_post_cover_text; ?></span>
         </a>
